10fee523df67812c77f6c0689939eaccb1e0c0da
egblanco <egblanco.betasoft@gmail.com>
2017-09-11 21:53:55 -0500

    Cambios en la pagina de inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>LaravelCPANEL GIT</title>
+        <title>APP SINCRONIZADA CON GIT</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
